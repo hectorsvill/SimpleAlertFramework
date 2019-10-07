d17bd3e89b0ed068f63768d3b5534dac99638e55
hectorsvill <hectorstevenvillasano@gmail.com>
2019-10-07 14:45:58 -0700

    Initial Commit

---
+++ SimpleAlert/SimpleAlert.h
@@ -0,0 +1,19 @@
+//
+//  SimpleAlert.h
+//  SimpleAlert
+//
+//  Created by Hector on 10/7/19.
+//  Copyright © 2019 Hector. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SimpleAlert.
+FOUNDATION_EXPORT double SimpleAlertVersionNumber;
+
+//! Project version string for SimpleAlert.
+FOUNDATION_EXPORT const unsigned char SimpleAlertVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SimpleAlert/PublicHeader.h>
+
+

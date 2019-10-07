76a6663261d1ce537e1f294d9e1b0f44e1577724
hectorsvill <hectorstevenvillasano@gmail.com>
2019-10-07 14:53:11 -0700

    actionSheetCanecel

--- SimpleAlert/SimpleAlert.swift
+++ SimpleAlert/SimpleAlert.swift
@@ -6,4 +6,18 @@
 //  Copyright © 2019 Hector. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+
+public class Service {
+    
+    private init() {}
+    
+    
+    public static func actionSheetCanecel(title: String, actionTitle: String) -> UIAlertController{
+        let alertController = UIAlertController(title: title, message: nil, preferredStyle: .actionSheet)
+        alertController.addAction(UIAlertAction(title: actionTitle, style: .cancel))
+        return alertController
+    }
+    
+}
